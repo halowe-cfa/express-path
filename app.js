65bb29e73ef348c1d6094c2a3cfca7a3e7061b03
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-29 11:59:29 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//open terminal
+//type npm init
+//type npm install express
+
+//create a variable called express and require 'express'
+
+//create a variable called app and set it equal to express()
+
+//create a variable called port and set it equal to 3000
+
+//create a get route (app.route) and use res.send to send text
+
+//make sure that your app is listening on the port
+
+//console.log `app listening on ${port}
+
+//run the app from the terminal (node app.js)
